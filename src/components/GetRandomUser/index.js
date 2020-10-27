8f7760712922410aefb2d91e9db4f083d5bda473
Daniel.Yu <adam741963@gmail.com>
2020-10-27 15:28:48 -0700

    get randomuser

---
+++ src/components/GetRandomUser/index.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import API from '../../utils/API';
+
+
+class GetRandomUser extends Component {
+  state = {
+    result: []
+  };
+
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default GetRandomUser;
---
+++ src/components/ResultList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ResultList() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ResultList;
