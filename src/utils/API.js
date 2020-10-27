ced12a30074d71a08a80fe7b53b4492053fab4c4
Daniel.Yu <adam741963@gmail.com>
2020-10-27 15:19:53 -0700

    API setup - 2

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 
 export default {
+  // Gets random user every time load the page
+  getRandomUser: function() {
+    return axios.get('https://randomuser.me/api/?nat=us');
+  },
   // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
+  getAllUsers: function() {
+    return axios.get('https://randomuser.me/api/?results=200&nat=us');
+  },
+  // Gets users by gender
+  getUsersByGender: function(gender) {
+    return axios.get('https://randomuser.me/api/?results=200&nat=us&gender=' + gender);
+  },
+
+
 };
